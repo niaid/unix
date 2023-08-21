61c2662bb4d54f1b584bbc403d62095270b96589
pooranibcbb <42645335+pooranibcbb@users.noreply.github.com>
2023-08-21 11:50:52 -0400

    add scripts for fall 2023 class

---
+++ job_script.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Job Name
+#$ -N fastqc
+
+# Execute the script from the Current Working Directory
+#$ -cwd
+
+# Merge the output of the script, and any error messages generated to one file
+#$ -j y
+
+# Tell the job your memory requirements
+#$ -l mem_free=2G,h_vmem=8G
+
+# Send mail when the job is submitted, and when the job completes
+#$ -m be
+
+#  Specify an email address to use
+#$ -M your_address@niaid.nih.gov
+
+
+
+
+module load FastQC/0.11.9-Java-1.8.0_45
+
+fastqc 22057_S2_R1_subsample.fastq.gz
+fastqc 22057_S2_R2_subsample.fastq.gz
+
+module purge
+
+module load MultiQC/1.12
+
+multiqc .
+
+
+sleep 200
